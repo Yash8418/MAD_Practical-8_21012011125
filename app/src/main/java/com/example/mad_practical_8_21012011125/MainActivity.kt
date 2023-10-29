7be6d4660da4b9214990a5037ebbeefad74f538d
Yash <y.d.prajapati04@gmail.com>
2023-10-29 16:46:00 +0530

    updated

--- app/src/main/java/com/example/mad_practical_8_21012011125/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_8_21012011125/MainActivity.kt
@@ -19,16 +19,15 @@ class MainActivity : AppCompatActivity() {
         cardView.visibility= View.GONE
         val createAlarm = findViewById<MaterialButton>(R.id.button_Alarm)
         createAlarm.setOnClickListener{
+
+            TimePickerDialog(this, { tp, hour, minute -> setAlarmTime(hour, minute) },Calendar.HOUR, Calendar.MINUTE,false).show()
             //cardView.visibility=View.VISIBLE
-            TimePickerDialog(this, { tp, hour, minute -> setAlarmTime(hour, minute) },
-                Calendar.HOUR, Calendar.MINUTE,false).show()
         }
     }
-
     fun setAlarmTime(hour:Int,minute:Int)
     {
         val cardView=findViewById<MaterialCardView>(R.id.card2)
-        cardView.visibility=View.GONE
+        cardView.visibility=View.VISIBLE
         val alarmtime=Calendar.getInstance()
         val year=alarmtime.get(Calendar.YEAR)
         val month=alarmtime.get(Calendar.MONTH)
@@ -47,7 +46,7 @@ class MainActivity : AppCompatActivity() {
     fun setAlarm(millitime:Long,action:String){
         val intentAlarm=Intent(applicationContext,AlarmBroadcastReceiver::class.java)
         intentAlarm.putExtra(AlarmBroadcastReceiver.ALARMKEY,action)
-        val pendingIntent=PendingIntent.getBroadcast(applicationContext,2345,intentAlarm,PendingIntent.FLAG_UPDATE_CURRENT)
+        val pendingIntent=PendingIntent.getBroadcast(applicationContext,2345,intentAlarm,PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
         val alarmmanager=getSystemService(ALARM_SERVICE) as AlarmManager
         if(action==AlarmBroadcastReceiver.ALARM_START){
             alarmmanager.setExact(AlarmManager.RTC_WAKEUP,millitime,pendingIntent)
